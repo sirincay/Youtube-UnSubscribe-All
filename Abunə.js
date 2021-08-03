592235796c39dbe9ba6e4d54e0260f89737668cf
Hüsnü Əhədov <80684361+sirincay@users.noreply.github.com>
2021-08-03 14:44:34 +0400

    Create Abunə.js

---
+++ Abunə.js
@@ -0,0 +1,27 @@
+var i = 0;
+var c = document.querySelectorAll("ytd-channel-renderer:not(.ytd-item-section-renderer)").length;
+
+husnuEhedov();
+
+function husnuEhedov () {	
+    if (c == 0) return;
+
+    el = document.querySelector('.ytd-subscribe-button-renderer');
+    el.click();
+
+    setTimeout(function () {
+        var unSubBtn = document.getElementById("confirm-button").click();
+        i++;
+        c--;
+
+        console.log(i + " abunəlikdən çıxıldı"); // Konsola göstərəcək
+        console.log(c + " abunə qaldı");
+
+        setTimeout(function () {
+            el = document.querySelector("ytd-channel-renderer");
+            el.parentNode.removeChild(el);
+
+            husnuEhedov();
+        }, 250);
+    }, 250);
+}
